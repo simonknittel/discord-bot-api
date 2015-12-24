e0b4ec3959d123ee5a9a1c09afc15a20bb737f6d
Simon Knittel <hallo@simonknittel.de>
2015-12-24 13:49:25 +0100

    Refactored to see better which commands get added

--- _modules/bot.js
+++ _modules/bot.js
@@ -43,49 +43,55 @@ function handleMessage(user, userID, channelID, message, rawEvent) {
     commands[requestedCommand].fn(user, userID, channelID, message.substring(message.split(' ')[0].length).trim(), rawEvent);
 }
 
+function aboutCommand(user, userID, channelID) {
+    bot.sendMessage({
+        to: channelID,
+        message: 'Hey there, I\'m a bot made by Simon Knittel (<hallo@simonknittel.de>). My functionality based on the Node.js library called discord.io (<https://github.com/izy521/discord.io>). My main feature is to offer a API for plugins that can be used with me. Visit <' + packageJSON.homepage + '> for more information. If you find bugs or have other issues please report them here <' + packageJSON.bugs.url + '>',
+    });
+}
+
+function commandsCommand(user, userID, channelID) {
+    let string = '';
+    Object.keys(commands).forEach(command => {
+        string += '`' + config.commandPrefix + command + '`' + (commands[command].description.length > 0 ? ' - ' + commands[command].description : '') + '\n';
+    });
+
+    bot.sendMessage({
+        to: channelID,
+        message: string,
+    });
+}
+
+function renameCommand(user, userID) {
+    if (userID !== config.ownerID) {
+        return false;
+    }
+
+    setName(bot, message);
+}
+
+function killCommand(user, userID) {
+    if (userID !== config.ownerID) {
+        return false;
+    }
+
+    console.log('The Discord Bot got stopped through the kill command.');
+    process.exit();
+}
+
+function userIDCommand(user, userID, channelID) {
+    bot.sendMessage({
+        to: channelID,
+        message: 'Your ID:' + userID,
+    });
+}
+
 function addGeneralCommands() {
-    bot.addCommand('about', (user, userID, channelID) => {
-        bot.sendMessage({
-            to: channelID,
-            message: 'Hey there, I\'m a bot made by Simon Knittel (<hallo@simonknittel.de>). My functionality based on the Node.js library called discord.io (<https://github.com/izy521/discord.io>). My main feature is to offer a API for plugins that can be used with me. Visit <' + packageJSON.homepage + '> for more information. If you find bugs or have other issues please report them here <' + packageJSON.bugs.url + '>',
-        });
-    }, 'Shows a short description of the bot');
-
-    bot.addCommand('commands', (user, userID, channelID) => {
-        let string = '';
-        Object.keys(commands).forEach(command => {
-            string += '`' + config.commandPrefix + command + '`' + (commands[command].description.length > 0 ? ' - ' + commands[command].description : '') + '\n';
-        });
-
-        bot.sendMessage({
-            to: channelID,
-            message: string,
-        });
-    }, 'Shows all available commands');
-
-    bot.addCommand('rename', (user, userID) => {
-        if (userID !== config.ownerID) {
-            return false;
-        }
-
-        setName(bot, message);
-    }, 'Renames the bot');
-
-    bot.addCommand('kill', (user, userID) => {
-        if (userID !== config.ownerID) {
-            return false;
-        }
-
-        console.log('The Discord Bot got stopped through the kill command.');
-        process.exit();
-    }, 'Stops the bot');
-
-    bot.addCommand('userid', (user, userID, channelID) => {
-        bot.sendMessage({
-            to: channelID,
-            message: 'Your ID:' + userID,
-        });
-    }, 'Displays the ID of the user');
+    bot.addCommand('about', aboutCommand, 'Shows a short description of the bot');
+    bot.addCommand('commands', commandsCommand, 'Shows all available commands');
+    bot.addCommand('rename', renameCommand, 'Renames the bot');
+    bot.addCommand('kill', killCommand, 'Stops the bot');
+    bot.addCommand('userid', userIDCommand, 'Displays the ID of the user');
 }
 
 bot.addCommand = (command, fn, description = '') => {
--- _plugins/music-bot/index.js
+++ _plugins/music-bot/index.js
@@ -38,142 +38,151 @@ function playLoop(channelID) {
     }
 }
 
-function addCommands() {
-    bot.addCommand('add', (user, userID, channelID, message) => {
-        const url = message.split(' ')[0];
+function addCommand(user, userID, channelID, message) {
+    const url = message.split(' ')[0];
 
-        bot.sendMessage({
-            to: channelID,
-            message: 'Downloading the requested song now.',
-        });
+    bot.sendMessage({
+        to: channelID,
+        message: 'Downloading the requested song now.',
+    });
 
-        pully({
-            url: url,
-            preset: 'audio',
-        }, (error, info, filePath) => {
-            if (error) {
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'The download of the song <' + url + '> failed.',
-                });
-            } else {
-                playlist.push({
-                    url: url,
-                    file: filePath,
-                });
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'The song <' + url + '> downloaded and added to the playlist. It\'s now on position ' + playlist.length + '.',
-                });
-            }
-        });
-    }, 'Adds a song to the playlist (Example: `' + config.commandPrefix + 'add https://www.youtube.com/watch?v=iyqfHvoUtkU`)');
-
-    bot.addCommand('remove', (user, userID, channelID, message) => {
-        const url = message.split(' ')[0];
-
-        playlist = playlist.filter(element => element.url !== url);
-
-        bot.sendMessage({
-            to: channelID,
-            message: 'Song removed from the playlist.',
-        });
-    }, 'Removes a song from the playlist (Example: `' + config.commandPrefix + 'remove https://www.youtube.com/watch?v=iyqfHvoUtkU`)');
-
-    bot.addCommand('skip', (user, userID, channelID) => {
-        if (voiceChannelID) {
-            bot.testAudio({channel: voiceChannelID, stereo: true}, stream => {
-                stream.stopAudioFile();
-                currentSong = null;
-            });
-
-            setTimeout(() => {
-                playLoop(channelID);
-            }, 2000);
-        } else {
+    pully({
+        url: url,
+        preset: 'audio',
+    }, (error, info, filePath) => {
+        if (error) {
             bot.sendMessage({
                 to: channelID,
-                message: 'The bot is not in a voice channel.',
-            });
-        }
-    }, 'Skips the current song');
-
-    bot.addCommand('enter', (user, userID, channelID, message) => {
-        let notFound = true;
-        Object.keys(bot.servers[config.serverID].channels).forEach((id) => {
-            const channel = bot.servers[config.serverID].channels[id];
-
-            if (channel.name === message && channel.type === 'voice') {
-                voiceChannelID = id;
-                notFound = false;
-            }
-        });
-
-        if (notFound) {
-            bot.sendMessage({
-                to: channelID,
-                message: 'There is no channel named ' + message + '.',
+                message: 'The download of the song <' + url + '> failed.',
             });
         } else {
-            bot.joinVoiceChannel(voiceChannelID);
-        }
-    }, 'Let the bot enter a voice channel (Example: `' + config.commandPrefix + 'enter Channel name`)');
-
-    bot.addCommand('play', (user, userID, channelID) => {
-        if (!voiceChannelID) {
+            playlist.push({
+                url: url,
+                file: filePath,
+            });
             bot.sendMessage({
                 to: channelID,
-                message: 'The bot is not in a voice channel.',
+                message: 'The song <' + url + '> downloaded and added to the playlist. It\'s now on position ' + playlist.length + '.',
             });
-        } else if (playlist.length <= 0) {
-            bot.sendMessage({
-                to: channelID,
-                message: 'There are currently no songs on the playlist.',
-            });
-        } else {
-            playLoop(channelID);
         }
-    }, 'Starts the playlist');
+    });
+}
 
-    bot.addCommand('stop', () => {
+function removeCommand(user, userID, channelID, message) {
+    const url = message.split(' ')[0];
+
+    playlist = playlist.filter(element => element.url !== url);
+
+    bot.sendMessage({
+        to: channelID,
+        message: 'Song removed from the playlist.',
+    });
+}
+
+function skipCommand(user, userID, channelID) {
+    if (voiceChannelID) {
         bot.testAudio({channel: voiceChannelID, stereo: true}, stream => {
             stream.stopAudioFile();
             currentSong = null;
         });
-    }, 'Stops the playlist');
 
-    bot.addCommand('current', (user, userID, channelID) => {
-        if (currentSong) {
-            bot.sendMessage({
-                to: channelID,
-                message: 'Currently playing: ' + currentSong.url,
-            });
-        } else {
-            bot.sendMessage({
-                to: channelID,
-                message: 'There is currently no song playing.',
-            });
-        }
-    }, 'Displays the current song');
+        setTimeout(() => {
+            playLoop(channelID);
+        }, 2000);
+    } else {
+        bot.sendMessage({
+            to: channelID,
+            message: 'The bot is not in a voice channel.',
+        });
+    }
+}
 
-    bot.addCommand('playlist', (user, userID, channelID) => {
-        if (playlist.length < 1) {
-            bot.sendMessage({
-                to: channelID,
-                message: 'There are currently no songs on the playlist.',
-            });
-        } else {
-            let string = '';
-            for (const song of playlist) {
-                string += ', ' + song.url;
-            }
-            string = string.substring(1);
-            bot.sendMessage({
-                to: channelID,
-                message: 'Current playlist: ' + string,
-            });
+function enterCommand(user, userID, channelID, message) {
+    let notFound = true;
+    Object.keys(bot.servers[config.serverID].channels).forEach((id) => {
+        const channel = bot.servers[config.serverID].channels[id];
+
+        if (channel.name === message && channel.type === 'voice') {
+            voiceChannelID = id;
+            notFound = false;
         }
-    }, 'Displays all songs on the playlist');
+    });
+
+    if (notFound) {
+        bot.sendMessage({
+            to: channelID,
+            message: 'There is no channel named ' + message + '.',
+        });
+    } else {
+        bot.joinVoiceChannel(voiceChannelID);
+    }
+}
+
+function playCommand(user, userID, channelID) {
+    if (!voiceChannelID) {
+        bot.sendMessage({
+            to: channelID,
+            message: 'The bot is not in a voice channel.',
+        });
+    } else if (playlist.length <= 0) {
+        bot.sendMessage({
+            to: channelID,
+            message: 'There are currently no songs on the playlist.',
+        });
+    } else {
+        playLoop(channelID);
+    }
+}
+
+function stopCommand() {
+    bot.testAudio({channel: voiceChannelID, stereo: true}, stream => {
+        stream.stopAudioFile();
+        currentSong = null;
+    });
+}
+
+function currentCommand(user, userID, channelID) {
+    if (currentSong) {
+        bot.sendMessage({
+            to: channelID,
+            message: 'Currently playing: ' + currentSong.url,
+        });
+    } else {
+        bot.sendMessage({
+            to: channelID,
+            message: 'There is currently no song playing.',
+        });
+    }
+}
+
+function playlistCommand(user, userID, channelID) {
+    if (playlist.length < 1) {
+        bot.sendMessage({
+            to: channelID,
+            message: 'There are currently no songs on the playlist.',
+        });
+    } else {
+        let string = '';
+        for (const song of playlist) {
+            string += ', ' + song.url;
+        }
+        string = string.substring(1);
+        bot.sendMessage({
+            to: channelID,
+            message: 'Current playlist: ' + string,
+        });
+    }
+}
+
+function addCommands() {
+    bot.addCommand('add', addCommand, 'Adds a song to the playlist (Example: `' + config.commandPrefix + 'add https://www.youtube.com/watch?v=iyqfHvoUtkU`)');
+    bot.addCommand('remove', removeCommand, 'Removes a song from the playlist (Example: `' + config.commandPrefix + 'remove https://www.youtube.com/watch?v=iyqfHvoUtkU`)');
+    bot.addCommand('skip', skipCommand, 'Skips the current song');
+    bot.addCommand('enter', enterCommand, 'Let the bot enter a voice channel (Example: `' + config.commandPrefix + 'enter Channel name`)');
+    bot.addCommand('play', playCommand, 'Starts the playlist');
+    bot.addCommand('stop', stopCommand, 'Stops the playlist');
+    bot.addCommand('current', currentCommand, 'Displays the current song');
+    bot.addCommand('playlist', playlistCommand, 'Displays all songs on the playlist');
 }
 
 addCommands();
