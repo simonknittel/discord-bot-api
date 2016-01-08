ff83d7d4b88bb14974d653a423bf2123014c42b2
Simon Knittel <hallo@simonknittel.de>
2016-01-08 19:50:36 +0100

    Commented out not used code

--- plugins/general/index.js
+++ plugins/general/index.js
@@ -49,62 +49,62 @@ function commandsCommand(user, userID, channelID) {
     });
 }
 
-function configCommand(user, userID, channelID, message) {
-    message = message.split(' ');
-
-    // Check if a property and a new value is present
-    if (message.length < 2) {
-        bot.sendMessage({
-            to: channelID,
-            message: 'Example use of this command: `!config credentials.name The Best Bot`',
-        });
-
-        return false;
-    }
-
-    let changingProperty = message[0].split('.');
-    message.shift(); // Remove the property from the message
-    let newValue = message.join(' ');
-    if (newValue === 'true') { // Make a boolean to a true boolean type
-        newValue = true;
-    } else if (newValue === 'false') { // Make a boolean to a true boolean type
-        newValue = false;
-    } else if (isNaN(newValue)) { // Make a number to a true number type
-        newValue = newValue;
-    } else {
-        newValue = Number(newValue);
-    }
-
-    // Create a object
-    let newConfig = {};
-    for (let i = changingProperty.length - 1; i >= 0; i--) {
-        if (i === changingProperty.length - 1) {
-            newConfig[changingProperty[i]] = newValue;
-        } else {
-            let lastSegment = JSON.parse(JSON.stringify(newConfig));
-            newConfig = {};
-            newConfig[changingProperty[i]] = lastSegment;
-        }
-    }
-
-    // Save the new config
-    configModule.save(newConfig, error => {
-        if (error) {
-            console.error(error);
-            bot.sendMessage({
-                to: channelID,
-                message: 'There was a problem with saving the new config.',
-            });
-
-            return false;
-        }
-
-        bot.sendMessage({
-            to: channelID,
-            message: 'Config successfully changed.',
-        });
-    });
-}
+// function configCommand(user, userID, channelID, message) {
+//     message = message.split(' ');
+//
+//     // Check if a property and a new value is present
+//     if (message.length < 2) {
+//         bot.sendMessage({
+//             to: channelID,
+//             message: 'Example use of this command: `!config credentials.name The Best Bot`',
+//         });
+//
+//         return false;
+//     }
+//
+//     let changingProperty = message[0].split('.');
+//     message.shift(); // Remove the property from the message
+//     let newValue = message.join(' ');
+//     if (newValue === 'true') { // Make a boolean to a true boolean type
+//         newValue = true;
+//     } else if (newValue === 'false') { // Make a boolean to a true boolean type
+//         newValue = false;
+//     } else if (isNaN(newValue)) { // Make a number to a true number type
+//         newValue = newValue;
+//     } else {
+//         newValue = Number(newValue);
+//     }
+//
+//     // Create a object
+//     let newConfig = {};
+//     for (let i = changingProperty.length - 1; i >= 0; i--) {
+//         if (i === changingProperty.length - 1) {
+//             newConfig[changingProperty[i]] = newValue;
+//         } else {
+//             let lastSegment = JSON.parse(JSON.stringify(newConfig));
+//             newConfig = {};
+//             newConfig[changingProperty[i]] = lastSegment;
+//         }
+//     }
+//
+//     // Save the new config
+//     configModule.save(newConfig, error => {
+//         if (error) {
+//             console.error(error);
+//             bot.sendMessage({
+//                 to: channelID,
+//                 message: 'There was a problem with saving the new config.',
+//             });
+//
+//             return false;
+//         }
+//
+//         bot.sendMessage({
+//             to: channelID,
+//             message: 'Config successfully changed.',
+//         });
+//     });
+// }
 
 function killCommand() {
     bot.disconnect();
